4acf9efc8a9bebba8f148ebeff255cb803a58f2b
dnautiyal <deepaknautiyal@hotmail.com>
2022-11-11 10:10:45 -0500

    a

--- webapp/client/routes_old.py
+++ webapp/client/routes_old.py
@@ -194,22 +194,59 @@ def allowed_video_file(filename):
 
 @app.route("/display/<path:filename>")
 def display_video(filename):
-    video_path = os.path.join(tmp_file_folder, filename)
+    path = os.path.join(tmp_file_folder, filename)
     # path = url_for('static', filename='tmp_data/' + filename)
-    return Response(gen_frames(video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
+    start, end = get_range(request)
+    return partial_response(path, start, end)
 
-# https://stackoverflow.com/questions/72367168/is-it-possible-to-use-opencv-to-display-a-video-on-web-browser-while-blurring-th
-def gen_frames(video_file_path):  # generate frame by frame from camera
-    camera = cv2.VideoCapture(video_file_path) 
-    print(f'gen_frames from {video_file_path}')
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+def partial_response(path, start, end=None):
+    logger.info('Requested: %s, %s', start, end)
+    file_size = os.path.getsize(path)
+
+    # Determine (end, length)
+    if end is None:
+        end = start + BUFF_SIZE - 1
+    end = min(end, file_size - 1)
+    end = min(end, start + BUFF_SIZE - 1)
+    length = end - start + 1
+
+    # Read file
+    with open(path, 'rb') as fd:
+        fd.seek(start)
+        bytes = fd.read(length)
+    assert len(bytes) == length
+
+    response = Response(
+        bytes,
+        206,
+        mimetype=mimetypes.guess_type(path)[0],
+        direct_passthrough=True,
+    )
+    response.headers.add(
+        'Content-Range', 'bytes {0}-{1}/{2}'.format(
+            start, end, file_size,
+        ),
+    )
+    response.headers.add(
+        'Accept-Ranges', 'bytes'
+    )
+    logger.info('Response: %s', response)
+    logger.info('Response: %s', response.headers)
+    return response
+
+def get_range(request):
+    range = request.headers.get('Range')
+    logger.info('Requested: %s', range)
+    m = re.match('bytes=(?P<start>\d+)-(?P<end>\d+)?', range)
+    if m:
+        start = m.group('start')
+        end = m.group('end')
+        start = int(start)
+        if end is not None:
+            end = int(end)
+        return start, end
+    else:
+        return 0, None
+
+
 
